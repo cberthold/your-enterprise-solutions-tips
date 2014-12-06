be59d833fd8251b6e44ca07b5aae2fa31aab2257
Chris Berthold <cberthold@technodotnet.com>
2014-12-06 17:30:57 -0500

    add resource factory pointing at random user generator api

--- angularjs/modal-promise/ModalPromise.Web/app/services.js
+++ angularjs/modal-promise/ModalPromise.Web/app/services.js
@@ -1,2 +1,10 @@
 ﻿var services = angular.module('tipServices', ['ngResource']);
 
+services.factory('Contact', ['$resource',
+    function ($resource) {
+        return $resource('http://api.randomuser.me/', {}, {
+            query: { method: 'GET', params: { results: 10 }, isArray: false },
+            single: { method: 'GET', params: { results: 1 }, isArray: false }
+        });
+    }
+]);
