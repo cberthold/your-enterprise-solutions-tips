f7654b180ced42d2f14cb3f5d83bece94e6d1d9b
Chris Berthold <cberthold@technodotnet.com>
2014-12-09 21:22:32 -0500

    base project

---
+++ owin-mvc-webapi/webapi-authentication/WebApiAuthentication/WebApiAuthentication.Api/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace WebApiAuthentication.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ owin-mvc-webapi/webapi-authentication/WebApiAuthentication/WebApiAuthentication.Api/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace WebApiAuthentication.Api.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
