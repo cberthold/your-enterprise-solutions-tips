adb4a6a97fdc199afec4f6ce370443453e8a248a
Chris Berthold <cberthold@technodotnet.com>
2014-12-17 21:38:36 -0500

    add nuget packages for web api and configure owin startup

---
+++ owin-mvc-webapi/webapi-authentication/WebApiAuthentication/WebApiAuthentication.Api/App_Start/Startup.Authenication.cs
@@ -0,0 +1,16 @@
+﻿using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiAuthentication.Api.App_Start
+{
+    public partial class Startup
+    {
+        private void ConfigureAuthentication(IAppBuilder app)
+        {
+            
+        }
+    }
+}
---
+++ owin-mvc-webapi/webapi-authentication/WebApiAuthentication/WebApiAuthentication.Api/App_Start/Startup.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Cors;
+using Microsoft.Owin.Cors;
+using System.Web.Http;
+using WebApiAuthentication.Business.Common;
+
+[assembly: OwinStartup(typeof(WebApiAuthentication.Api.App_Start.Startup))]
+
+namespace WebApiAuthentication.Api.App_Start
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            // get current settings
+            var settings = AppConfiguration.Current;
+            // setup cors to accept all for now
+            app.UseCors(CorsOptions.AllowAll);
+            // create new http configuration and register web api configurations
+            var config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            // configure authenticate
+            ConfigureAuthentication(app);
+
+            //app.UseStageMarker(PipelineStage.Authenticate); // Needed for IIS pipeline
+            // get owin to use the web api configuration
+            app.UseWebApi(config);
+        }
+    }
+}
