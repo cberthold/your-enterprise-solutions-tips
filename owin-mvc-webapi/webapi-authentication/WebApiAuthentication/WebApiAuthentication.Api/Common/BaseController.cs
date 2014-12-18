bc0fb933568c79766ef43b26d749cbbba145f7c3
Chris Berthold <cberthold@technodotnet.com>
2014-12-17 21:58:02 -0500

    add base controller for mvc controllers

---
+++ owin-mvc-webapi/webapi-authentication/WebApiAuthentication/WebApiAuthentication.Api/Common/BaseController.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApiAuthentication.Api.Common
+{
+    public abstract class BaseController : WebApiAuthentication.Common.Mvc.BaseController
+    {
+    }
+}
