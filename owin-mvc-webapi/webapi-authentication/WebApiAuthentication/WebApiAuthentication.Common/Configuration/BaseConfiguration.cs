61573eba980d94cb11b7d9354ea76dc34232850a
Chris Berthold <cberthold@technodotnet.com>
2014-12-18 00:58:17 -0500

    simplify and extend types in base configuration

--- owin-mvc-webapi/webapi-authentication/WebApiAuthentication/WebApiAuthentication.Common/Configuration/BaseConfiguration.cs
+++ owin-mvc-webapi/webapi-authentication/WebApiAuthentication/WebApiAuthentication.Common/Configuration/BaseConfiguration.cs
@@ -17,6 +17,72 @@ namespace WebApiAuthentication.Common.Configuration
         private ConcurrentDictionary<string, object> cacheObject = new ConcurrentDictionary<string, object>();
         private Type thisType = typeof(TConfig);
 
+        protected T GetAndStoreValue<T>(Expression<Func<TConfig, T>> configProperty, T defaultValue = default(T))
+        {
+            // try or get property via action
+            Func<string, T> action = (prop) =>
+            {
+                object value = defaultValue;
+
+                var type = typeof(T);
+                var valueType = type;
+                var isNullable = false;
+
+                if (type.IsGenericType && type.GetGenericTypeDefinition() == typeof(Nullable<>))
+                {
+                    valueType = type.GetGenericArguments()[0];
+                    isNullable = true;
+                }
+
+                if (valueType == typeof(string))
+                {
+                    value = GetString(prop);
+                }
+                else if (valueType == typeof(int))
+                {
+                    value = GetInteger(prop);
+                }
+                else if (valueType == typeof(bool))
+                {
+                    value = GetBoolean(prop);
+                }
+                else if (valueType == typeof(decimal))
+                {
+                    value = GetDecimal(prop);
+                }
+                else if (valueType == typeof(double))
+                {
+                    value = GetDouble(prop);
+                }
+                else if (valueType == typeof(Guid))
+                {
+                    value = GetGuid(prop);
+                }
+                else if (valueType == typeof(float))
+                {
+                    value = GetFloat(prop);
+                }
+                else
+                {
+                    throw new InvalidOperationException();
+                }
+
+                // value returned null so check if we should set default value
+                if (value == null)
+                {
+                    if (isNullable && defaultValue == null)
+                        return default(T);
+
+                    value = defaultValue;
+                }
+
+                return (T)value;
+            };
+
+            return GetAndStoreValue(configProperty, action);
+
+        }
+
         protected T GetAndStoreValue<T>(Expression<Func<TConfig, T>> configProperty, Func<string, T> action)
         {
             // get the configuration property
@@ -29,29 +95,16 @@ namespace WebApiAuthentication.Common.Configuration
             // get the property name from the memberinfo
             var propertyName = property.Name;
 
-            // try or get property via action
-            var returnValue = (T)cacheObject.GetOrAdd(propertyName, (propName) =>
+            // get the cached value or create new value via action passed in
+            T returnValue = (T)cacheObject.GetOrAdd(propertyName, (prop) =>
             {
-                return action.Invoke(propName);
+                return action.Invoke(prop);
             });
-            
+
             return returnValue;
 
         }
 
-
-
-        protected int GetOrDefaultInteger(string key, int defaultValue = 0)
-        {
-            int? intValue = GetInteger(key);
-
-            if (!intValue.HasValue)
-            {
-                intValue = defaultValue;
-            }
-
-            return intValue.Value;
-        }
 
         protected int? GetInteger(string key)
         {
@@ -65,18 +118,6 @@ namespace WebApiAuthentication.Common.Configuration
             }
 
             return null;
-        }
-
-        protected bool GetOrDefaultBoolean(string key, bool defaultValue = false)
-        {
-            bool? boolValue = GetBoolean(key);
-
-            if (!boolValue.HasValue)
-            {
-                boolValue = defaultValue;
-            }
-
-            return boolValue.Value;
         }
 
         protected bool? GetBoolean(string key)
@@ -93,18 +134,6 @@ namespace WebApiAuthentication.Common.Configuration
             return null;
         }
 
-        protected Guid GetOrDefaultGuid(string key, Guid defaultValue)
-        {
-            Guid? guidValue = GetGuid(key);
-
-            if (!guidValue.HasValue)
-            {
-                guidValue = defaultValue;
-            }
-
-            return guidValue.Value;
-        }
-
         protected Guid? GetGuid(string key)
         {
             string stringValue = GetApplicationKey(key);
@@ -119,35 +148,11 @@ namespace WebApiAuthentication.Common.Configuration
             return null;
         }
 
-        protected string GetOrDefaultstring(string key, string defaultValue = null)
-        {
-            string stringValue = GetString(key);
-
-            if (stringValue == null)
-            {
-                stringValue = defaultValue;
-            }
-
-            return stringValue;
-        }
-
         protected string GetString(string key)
         {
             string stringValue = GetApplicationKey(key);
 
             return stringValue;
-        }
-
-        protected decimal GetOrDefaultDecimal(string key, decimal defaultValue)
-        {
-            decimal? decimalValue = GetDecimal(key);
-
-            if (!decimalValue.HasValue)
-            {
-                decimalValue = defaultValue;
-            }
-
-            return decimalValue.Value;
         }
 
         protected decimal? GetDecimal(string key)
@@ -157,6 +162,34 @@ namespace WebApiAuthentication.Common.Configuration
             decimal parsedValue;
 
             if (decimal.TryParse(stringValue, out parsedValue))
+            {
+                return parsedValue;
+            }
+
+            return null;
+        }
+
+        protected double? GetDouble(string key)
+        {
+            string stringValue = GetApplicationKey(key);
+
+            double parsedValue;
+
+            if (double.TryParse(stringValue, out parsedValue))
+            {
+                return parsedValue;
+            }
+
+            return null;
+        }
+
+        protected float? GetFloat(string key)
+        {
+            string stringValue = GetApplicationKey(key);
+
+            float parsedValue;
+
+            if (float.TryParse(stringValue, out parsedValue))
             {
                 return parsedValue;
             }
